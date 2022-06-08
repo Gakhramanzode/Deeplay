5a92542027f0a52ca1089deea7adcf464c61a41d
Asker Kakhramanov <62985982+Gakhramanzode@users.noreply.github.com>
2022-06-08 14:33:11 +0300

    Create task-1.py
    Первое задание

---
+++ task-1.py
@@ -0,0 +1,25 @@
+import re
+
+# путь или название файла
+file = "/content/drive/MyDrive/log.txt"
+
+# получим объект файла
+with open(file, "r", encoding='utf-8') as log:
+    # считываем все строки
+    lines = log.readlines()
+
+# создаем массив, в который будем складывать все нужные нам IP
+array = []
+
+# задаем искомый IP (можно сделать интерактивным, чтобы пользователь сам вводил и повесить валидацию)
+IP = "10.1.192.38"
+
+# итерация по строкам
+for line in lines:
+    # ищем нужный IP
+    if IP in line:
+        regs = re.search('sid\=.*&type\=', line).regs[0]
+        array.append(line[regs[0] + 5:regs[1] - 7])
+
+for x in array:
+    print(x)
